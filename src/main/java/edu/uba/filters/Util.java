e44bb1f4dbc6fd4f1df85b429d69e4c9ba759aee
jamaal.taylor <j.jerrod.taylor@gmail.com>
2015-11-02 18:34:58 -0300

    Simple util class.

---
+++ src/main/java/edu/uba/filters/Util.java
@@ -0,0 +1,12 @@
+package edu.uba.filters;
+
+/**
+ * Created by jamaaltaylor on 11/1/15.
+ */
+public class Util {
+
+    public static String convertToString(Object o){
+        return (String) o;
+    }
+
+}
