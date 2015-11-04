746fa2db09e324f0831b14f8d3ea6bf65e40c168
jamaal.taylor <j.jerrod.taylor@gmail.com>
2015-11-04 14:43:46 -0300

    I changed the tests from the simple survive/sex example that I had and I used the birthweight data from class.

--- src/test/java/edu/uba/filters/EntropyTest.java
+++ src/test/java/edu/uba/filters/EntropyTest.java
@@ -1,9 +1,11 @@
 package edu.uba.filters;
 
+import edu.uba.filters.Tools.FileHelper;
 import org.junit.Before;
 import org.junit.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.*;
@@ -15,61 +17,92 @@ public class EntropyTest{
 
     @Test
     public void testEntropy(){
-        List<String> survived = Arrays.asList("1","0","1","1","0","1","0","0","0","1","0","1","0","0","1");
+        FileHelper fileHelper = new FileHelper();
+        List<String> lines = fileHelper.readFileToMemory("/Users/jamaaltaylor/Documents/datos/birthwt.txt");
+        Data freshData = fileHelper.parseCSVData(lines);
+
+        LinkedList<String> headersToChange = new LinkedList<String>();
+        headersToChange.add("lwt");
+
+        Data discreteData = freshData.discretize(freshData.getData(),headersToChange,1,10);
+
         Entropy entropy = new Entropy();
-        Double result = entropy.entropy(survived);
-        assertEquals(.996,result,.006);
+        Double result = entropy.entropy(discreteData.getData().get("lwt"));
+        assertEquals(2.48,result,.006);
     }
 
     @Test
     public void testConditionalProbability(){
-        //male = 1
-        //survived = 1
 
-        List<String> survived = Arrays.asList("1","0","1","1","0","1","0","0","0","1","0","1","0","0","1");
-        List<String> sex = Arrays.asList("0","1","0","1","1","0","0","1","1","0","1","0","0","1","1");
+        FileHelper fileHelper = new FileHelper();
+        List<String> lines = fileHelper.readFileToMemory("/Users/jamaaltaylor/Documents/datos/birthwt.txt");
+        Data freshData = fileHelper.parseCSVData(lines);
+
+        LinkedList<String> headersToChange = new LinkedList<String>();
+        headersToChange.add("age");
+        headersToChange.add("lwt");
+
+
+        Data discreteData = freshData.discretize(freshData.getData(), headersToChange, 1, 10);
+
         Entropy entropy = new Entropy();
-        double conditionalProb = entropy.conditionalProbability(survived,sex,"1","1");
-        assertEquals(.250,conditionalProb,.005);
+        double conditionalProb = entropy.conditionalProbability(discreteData.getData().get("lwt"),discreteData.getData().get("age"),"4","6");
+        assertEquals(.1,conditionalProb,.005);
     }
 
     @Test
     public void testJointEntropy(){
 
-        //male = 1
-        //survived = 1
 
-        List<String> survived = Arrays.asList("1","0","1","1","0","1","0","0","0","1","0","1","0","0","1");
-        List<String> sex = Arrays.asList("0","1","0","1","1","0","0","1","1","0","1","0","0","1","1");
+        FileHelper fileHelper = new FileHelper();
+        List<String> lines = fileHelper.readFileToMemory("/Users/jamaaltaylor/Documents/datos/birthwt.txt");
+        Data freshData = fileHelper.parseCSVData(lines);
+
+        LinkedList<String> headersToChange = new LinkedList<String>();
+        headersToChange.add("age");
+        headersToChange.add("lwt");
+
+        Data discreteData = freshData.discretize(freshData.getData(), headersToChange, 1, 10);
+
         Entropy entropy = new Entropy();
-        double jointEntropy = entropy.jointEntropy(survived,sex);
-        assertEquals(1.99,jointEntropy,.006);
+        double jointEntropy = entropy.jointEntropy(discreteData.getData().get("lwt"),discreteData.getData().get("age"));
+        assertEquals(5.05,jointEntropy,.006);
     }
 
     @Test
     public void testSpecifiedConditionalEntropy(){
 
-        //male = 1
-        //survived = 1
+        FileHelper fileHelper = new FileHelper();
+        List<String> lines = fileHelper.readFileToMemory("/Users/jamaaltaylor/Documents/datos/birthwt.txt");
+        Data freshData = fileHelper.parseCSVData(lines);
 
-        List<String> survived = Arrays.asList("1","0","1","1","0","1","0","0","0","1","0","1","0","0","1");
-        List<String> sex = Arrays.asList("0","1","0","1","1","0","0","1","1","0","1","0","0","1","1");
+        LinkedList<String> headersToChange = new LinkedList<String>();
+        headersToChange.add("age");
+        headersToChange.add("lwt");
+
+        Data discreteData = freshData.discretize(freshData.getData(), headersToChange, 1, 10);
+
         Entropy entropy = new Entropy();
-        double specCondiEntropy = entropy.specifiedConditionalEntropy(survived,sex,"1","1");
-        assertEquals(.5,specCondiEntropy,.005);
+        double specCondiEntropy = entropy.specifiedConditionalEntropy(discreteData.getData().get("lwt"),discreteData.getData().get("age"),"4","6");
+        assertEquals(.332,specCondiEntropy,.005);
 
     }
 
     @Test
     public void testConditionalEntropy(){
 
-        //male = 1
-        //survived = 1
+        FileHelper fileHelper = new FileHelper();
+        List<String> lines = fileHelper.readFileToMemory("/Users/jamaaltaylor/Documents/datos/birthwt.txt");
+        Data freshData = fileHelper.parseCSVData(lines);
 
-        List<String> survived = Arrays.asList("1","0","1","1","0","1","0","0","0","1","0","1","0","0","1");
-        List<String> sex = Arrays.asList("0","1","0","1","1","0","0","1","1","0","1","0","0","1","1");
+        LinkedList<String> headersToChange = new LinkedList<String>();
+        headersToChange.add("age");
+        headersToChange.add("lwt");
+
+        Data discreteData = freshData.discretize(freshData.getData(), headersToChange, 1, 10);
+
         Entropy entropy = new Entropy();
-        Double result = entropy.conditionalEntropy(survived,sex);
+        Double result = entropy.conditionalEntropy(discreteData.getData().get("lwt"),discreteData.getData().get("age"));
         assertEquals(.996,result,.006);
     }
 
